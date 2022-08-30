8cca1d9213a5ceb64c9973fcfd359331cf3495d7
Thibault Tubiana <tubiana.thibault@gmail.com>
2022-08-30 16:24:16 +0200

    mrccleaner initial commit

---
+++ mrccleaner.py
@@ -0,0 +1,82 @@
+__author__ = "Thibault TUBIANA"
+__license__ = "MIT"
+__date__ = "2022/08"
+__version__ = "1.0"
+
+import sys
+import subprocess
+
+def try_import_and_install(package:str, shortname=""):
+    """
+    Homemade function to try to import packages, and if they don't exist,
+    they will be automatically installed with pypi.
+    Args:
+        package (str): package name (example 'numpy')
+        shortname (str): shortname of the package (example : 'np' as for 'import numpy as np')
+    """
+    if shortname == "":
+        importname = package
+    else:
+        importname = shortname
+        
+        
+    try:
+        globals()[importname] = __import__(package)
+    except:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+        globals()[importname] = __import__(package)
+
+
+try_import_and_install("mrcfile")
+try_import_and_install("numpy","np")
+try_import_and_install("argparse")
+
+
+
+def parseArg():
+    """
+    Parse Arguments
+    @return: dictionnary of arguments
+    """
+    arguments = argparse.ArgumentParser(description="Simple script to recenter and symmetrize MRC file.")
+    arguments.add_argument('-f', "--file", help="Input MRC file", required=True, type=str)
+    arguments.add_argument('-o', '--output', help="Output file (mrc)", default="output.mrc")
+    
+    args = vars(arguments.parse_args())
+
+    return (args)
+
+def transform_mrc(inputFile:str, outputFile:str):
+    """
+    Read MRC files and transform them with 
+    - fftshift
+    - mirror + inversion
+    Args:
+        inputFile (str): input mrc file path
+        outputFile (str): Ouput mrc file path
+    """
+
+    with mrcfile.open(inputFile) as mrc:
+        im = mrc.data
+
+    shifted_mrc = np.fft.fftshift(im[:-1,:], axes=0)
+    #Now we flipp the centered shifted MRC file, and we invert it (because after A LOT of tries... This has to be done....)
+
+    correctMRC = np.concatenate((np.flip(shifted_mrc[::-1,::]), shifted_mrc), axis=1)
+    #Correct... Hopefully!
+
+    with mrcfile.new(outputFile, overwrite=True) as mrc:
+        mrc.set_data(correctMRC, )
+
+
+if __name__ == "__main__":
+    args = parseArg()
+
+    inputFile = args["file"]
+    outputFile = args["output"]
+
+    print("== Transforming output mrc ==")
+    print(f"---- Input file: {inputFile}")
+    print(f"---- Output file: {outputFile}")
+    transform_mrc(inputFile, outputFile)
+
